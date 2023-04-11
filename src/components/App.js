7f2f53dd09c32588deef2aef33df17074af1598e
Vincent BOLLOT <vincent.bollot@gmail.com>
2023-04-11 09:42:15 +0200

    update project react

---
+++ src/components/App.js
@@ -0,0 +1,40 @@
+//import logo from '../logo.svg';
+import "../App.css";
+import Banner from "./Banner";
+import Cart from "./Cart";
+import ShoppingList from "./ShoppingList";
+
+function App() {
+  return (
+    <div>
+      <Banner />
+      <Cart />
+      <ShoppingList />
+    </div>
+  );
+}
+
+export default App;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/components/ShoppingList.js
@@ -0,0 +1,27 @@
+import { plantList } from "../datas/plantList";
+
+function ShoppingList() {
+  let categories = plantList.reduce(
+    (acc, plant) =>
+      acc.includes(plant.category) ? acc : acc.concat(plant.category),
+    []
+  );
+
+  return (
+    <div>
+      <ul>
+        {categories.map((cat) => (
+          <li key={cat}>{cat}</li>
+        ))}
+      </ul>
+      <ul>
+        {plantList.map((plant) => (
+          <li key={plant.id}>{plant.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+
+}
+
+export default ShoppingList;
